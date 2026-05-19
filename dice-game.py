8600a4e2618e0e4dea69200808571dad55bb8b10
rabhussam <h.alrababaa@gmail.com>
2026-05-19 19:24:42 +0300

    Add dice game project

---
+++ dice-game.py
@@ -0,0 +1,41 @@
+import random
+
+cards = {
+        1: ("------------",
+            "|     1    |",
+            "____________"),
+        2: ("------------",
+            "|     2    |",
+            "____________"),
+        3: ("------------",
+            "|     3    |",
+            "____________"),
+        4: ("------------",
+            "|     4    |",
+            "____________"),
+        5: ("------------",
+            "|     5    |",
+            "____________")
+}
+
+total = 0
+showcase = []
+
+hits = int(input("How many cards would you like to hit: "))
+
+for hit in range(hits):
+    showcase.append(random.randint(1,5))
+
+for hit in range(hits):
+    for line in cards.get(showcase[hit]):
+       print(line)
+    print()
+
+#for line in range(3):
+#    for hit in showcase:
+#       print(cards.get(hit)[line], end="")
+#    print()
+for hit in showcase:
+    total += hit
+
+print(f"total: {total}")
